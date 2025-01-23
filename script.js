1dd1d95e44b1efdfd5dc197c0abaff6c7f09c7a5
Santiago Ramirez Niño <A01665906@tec.mx>
2025-01-22 18:12:20 -0600

    Project finished

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log(products)
+const user={
+    name: "Roberto",
+    age: 30,
+    debt: 0
+}
+
+let order = []
+let orderCost = 0
 
 const showMenu = () => {
     console.log("CODE: NAME - PRICE")
@@ -7,4 +14,44 @@ const showMenu = () => {
     }
 }
 
-showMenu()
+const askProduct = cod => {
+    if (!cod) return "Enter a valid code"
+    const search = products.find(product => product.code === cod)
+    if(!search) return "Product does not exist"
+    order.push(search)
+    console.log("Your product has been added to your order. Your order is: ")
+    return seeOrder()
+}
+
+const seeOrder = () => order
+
+const calculateCost = () => {
+    let cost = 0
+    for (product of order){
+        cost += product.price
+    }
+    orderCost = cost
+    return orderCost
+}
+
+const endOrder = () => {
+    calculateCost()
+    user.debt = orderCost
+    order = []
+    orderCost = 0
+    return `${user.name} you have to pay $${user.debt} dollars`
+}
+
+const payDebt = givenMoney => {
+    if (givenMoney < user.debt){
+        return "No funds"
+    } 
+    else if (givenMoney === user.debt){
+        return "Order paid"
+        user.debt = 0
+    }
+    else{
+        return `Order paid and your change is ${givenMoney - user.debt}`
+        user.debt = 0
+    }
+}
